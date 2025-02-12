4c2662c7ae5870f49748ca4c277a4ba5c232347f
Nishanth <mnishanthkumar2@gmail.com>
2025-02-12 18:16:55 +0530

    Fix a imgae not show in project card

--- src/info.js
+++ src/info.js
@@ -1,7 +1,7 @@
 const projects = [
     {
         id: 0,
-        image: "../public/assets/project_image/todo_app.png",
+        image: "./assets/project_image/todo_app.png",
         projectName: "Public Todo App (Fullstack)",
         projectAbout: "This project is a fullstack Todo App. The front-end is built with React and the back-end is powered by Python Django. Through this project, I learned how to develop an end-to-end fullstack application.",  
         liveLink: "https://todo-frontend-dev.netlify.app/",
@@ -9,7 +9,7 @@ const projects = [
     },
     {
         id: 1,
-        image: "../public/assets/project_image/shop_porul.png",
+        image: "./assets/project_image/shop_porul.png",
         projectName: "Shop Porul (E-commerce site)",
         projectAbout: "This project is a functional front-end e-commerce site built with pure HTML, CSS, and JavaScript. Through this project, I learned how to create dynamic web pages and manage state using JavaScript.",
         liveLink: "https://shop-porul.netlify.app/",
@@ -17,7 +17,7 @@ const projects = [
     },
     {
         id: 2,
-        image: "../public/assets/rock_paper_scissors.png",
+        image: "./assets/project_image/rock_paper_scissors.png",
         projectName: "Rock Paper Scissors Game",
         projectAbout: "This project is a Rock Paper Scissors game built with HTML, CSS, and JavaScript. Through this project, I learned how to manipulate the DOM and manage user interactions using JavaScript.",
         liveLink: "https://nishanthnaa52.github.io/rock_paper_scissors_project/",
@@ -25,14 +25,14 @@ const projects = [
     },
     {
         id: 3,
-        image: "../public/assets/project_image/sdal.png",
+        image: "./assets/project_image/sdal.png",
         projectName: "SDAL (Basic Desktop assistant)",
         projectAbout: "This project is a basic desktop assistant built with Python.",
         githubLink: "https://github.com/Nishanthnaa52/SDAL"
     },
     {
         id: 4,
-        image: "../public/assets/tic_tac_toe.png",
+        image: "./assets/project_image/tic_tac_toe.png",
         projectName: "Tic Tac Toe Game",
         projectAbout: "This project is a Tic Tac Toe game built with HTML, CSS, and JavaScript.",
         liveLink: "https://nishanthnaa52.github.io/tic_tac_toe_js/",
@@ -40,7 +40,7 @@ const projects = [
     },
     {
         id: 5,
-        image: "../public/assets/project_image/youtube_clone.png",
+        image: "./assets/project_image/youtube_clone.png",
         projectName: "Youtube Clone",
         projectAbout: "This project is a Youtube Clone built with pure HTML and CSS.",
         liveLink: "https://nishanthnaa52.github.io/_youtube_clone_/",
@@ -48,7 +48,7 @@ const projects = [
     },
     {
         id: 6,
-        image: "../public/assets/project_image/Calculator.png",
+        image: "./assets/project_image/Calculator.png",
         projectName: "Calculator",
         projectAbout: "This project is a Calculator built with HTML, CSS, and JavaScript.",
         liveLink: "https://www.instagram.com/nishanth_.dev/",
@@ -56,7 +56,7 @@ const projects = [
     },
     {
         id: 7,
-        image: "../public/assets/project_image/portfolio.png",
+        image: "./assets/project_image/portfolio.png",
         projectName: "Amazing Portfolio",
         projectAbout: "This project is my portfolio built with React and Tailwind CSS.",
         liveLink: "https://nishanth-portfolio-22.netlify.app/",
