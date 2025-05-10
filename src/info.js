6f27cf835c50fa8f91d122c0dfc52ae548c30257
Nishanth <98414644+Nishanthnaa52@users.noreply.github.com>
2025-05-10 13:53:38 +0530

    add a edumatric projecrt

--- src/info.js
+++ src/info.js
@@ -7,9 +7,16 @@ const projects = [
     //     liveLink: "https://todo-frontend-dev.netlify.app/",
     //     githubLink: "https://github.com/Nishanthnaa52/Todo-frontend"
     // },
-   
     {
         id: 0,
+        image: "./assets/project_image/edumetric.png",
+        projectName: "EduMetric 🎓",
+        projectAbout: "EduMatric GPA & CGPA Calculator is a simple web-based tool designed to help students calculate their semester-wise GPA and overall CGPA manually by entering subject names, credits, and grades. The app provides an easy-to-use interface and allows users to export their results as a professional-looking PDF report. It is especially useful for college students who want a quick and reliable way to track their academic performance without relying on external systems.",
+        liveLink: "https://edumetric.netlify.app/",
+        // githubLink: ""
+    },
+    {
+        id: 1,
         image: "./assets/project_image/emica25.png",
         projectName: "College Symposium site(EMICA25)",
         projectAbout: "I build a site for our college sympowsium event EMICA25. This site is built with react, tailwind css, etc. this site containe a data for all event detiales, time, place.,",
@@ -17,7 +24,7 @@ const projects = [
         // githubLink: ""
     },
     {
-        id: 0,
+        id: 2,
         image: "./assets/project_image/whatsToday22.png",
         projectName: "What's Today?(weather App)",
         projectAbout: "This project is a weather app built with React and Tailwind CSS also Integrate data into a graph chart ,Through this project, I learned how to work with APIs and manage state in React.",
@@ -25,7 +32,7 @@ const projects = [
         // githubLink: ""
     },
     {
-        id: 1,
+        id: 3,
         image: "./assets/project_image/shop_porul.png",
         projectName: "Shop Porul (E-commerce site)",
         projectAbout: "This project is a functional front-end e-commerce site built with pure HTML, CSS, and JavaScript. Through this project, I learned how to create dynamic web pages and manage state using JavaScript.",
@@ -33,7 +40,7 @@ const projects = [
         githubLink: "https://github.com/Nishanthnaa52/Shop_Porul"
     },
     {
-        id: 2,
+        id: 4,
         image: "./assets/project_image/rock_paper_scissors.png",
         projectName: "Rock Paper Scissors Game",
         projectAbout: "This project is a Rock Paper Scissors game built with HTML, CSS, and JavaScript. Through this project, I learned how to manipulate the DOM and manage user interactions using JavaScript.",
@@ -41,14 +48,14 @@ const projects = [
         githubLink: "https://github.com/Nishanthnaa52/rock_paper_scissors_project"
     },
     {
-        id: 3,
+        id: 5,
         image: "./assets/project_image/sdal.png",
         projectName: "SDAL (Basic Desktop assistant)",
         projectAbout: "This project is a basic desktop assistant built with Python.",
         githubLink: "https://github.com/Nishanthnaa52/SDAL"
     },
     {
-        id: 4,
+        id: 6,
         image: "./assets/project_image/tic_tac_toe.png",
         projectName: "Tic Tac Toe Game",
         projectAbout: "This project is a Tic Tac Toe game built with HTML, CSS, and JavaScript.",
@@ -56,7 +63,7 @@ const projects = [
         githubLink: "https://github.com/Nishanthnaa52/tic_tac_toe_js"
     },
     {
-        id: 5,
+        id: 7,
         image: "./assets/project_image/youtube_clone.png",
         projectName: "Youtube Clone",
         projectAbout: "This project is a Youtube Clone built with pure HTML and CSS.",
@@ -64,7 +71,7 @@ const projects = [
         githubLink: "https://github.com/Nishanthnaa52/_youtube_clone_"
     },
     {
-        id: 6,
+        id: 8,
         image: "./assets/project_image/Calculator.png",
         projectName: "Calculator",
         projectAbout: "This project is a Calculator built with HTML, CSS, and JavaScript.",
@@ -72,7 +79,7 @@ const projects = [
         githubLink: "https://github.com/Nishanthnaa52/Simple_Calculator"
     },
     {
-        id: 7,
+        id: 9,
         image: "./assets/project_image/portfolio.png",
         projectName: "Amazing Portfolio",
         projectAbout: "This project is my portfolio built with React and Tailwind CSS.",
