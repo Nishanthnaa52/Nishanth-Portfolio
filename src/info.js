69918faafa6454a424ff655e08e72f29cf0d6f86
Nishanth <98414644+Nishanthnaa52@users.noreply.github.com>
2025-07-20 21:53:55 +0530

    Update info.js

--- src/info.js
+++ src/info.js
@@ -8,7 +8,7 @@ const projects = [
     //     githubLink: "https://github.com/Nishanthnaa52/Todo-frontend"
     // },
     {
-        id: 00,
+        id: 0,
         image: "./assets/project_image/Talk2pdf.png",
         projectName: "Talk2Pdf",
         projectAbout: "Talk2PDF is a powerful Python Flask-based web application designed to transform static PDF documents into dynamic, interactive learning experiences. By integrating Google Gemini API, the app allows users to chat with any PDF in a natural language interface making it easy to extract information, ask questions, and deeply understand complex content. The application supports image extraction and explanation, enabling users to view images from the PDF in a dedicated section and receive detailed, AI-generated descriptions or context-based answers about those visuals. A built-in chat history section lets users revisit past interactions for reference or review, adding an extra layer of continuity and usability. With its modern, user-friendly interface, Talk2PDF is especially valuable for students, researchers, and digital readers who want to study smarter by engaging directly with documents. Whether it's understanding academic material, analyzing research papers, or reviewing lengthy PDFs, Talk2PDF turns passive reading into active learning.",
@@ -16,7 +16,7 @@ const projects = [
         githubLink: "https://github.com/Nishanthnaa52/Talk2pdf"
     },
     {
-        id: 0,
+        id: 1,
         image: "./assets/project_image/edumetric.png",
         projectName: "EduMetric 🎓",
         projectAbout: "EduMatric GPA & CGPA Calculator is a simple web-based tool designed to help students calculate their semester-wise GPA and overall CGPA manually by entering subject names, credits, and grades. The app provides an easy-to-use interface and allows users to export their results as a professional-looking PDF report. It is especially useful for college students who want a quick and reliable way to track their academic performance without relying on external systems.",
@@ -24,7 +24,7 @@ const projects = [
         // githubLink: ""
     },
     {
-        id: 1,
+        id: 2,
         image: "./assets/project_image/emica25.png",
         projectName: "College Symposium site(EMICA25)",
         projectAbout: "I build a site for our college sympowsium event EMICA25. This site is built with react, tailwind css, etc. this site containe a data for all event detiales, time, place.,",
@@ -32,7 +32,7 @@ const projects = [
         // githubLink: ""
     },
     {
-        id: 2,
+        id: 3,
         image: "./assets/project_image/whatsToday22.png",
         projectName: "What's Today?(weather App)",
         projectAbout: "This project is a weather app built with React and Tailwind CSS also Integrate data into a graph chart ,Through this project, I learned how to work with APIs and manage state in React.",
@@ -40,7 +40,7 @@ const projects = [
         // githubLink: ""
     },
     {
-        id: 3,
+        id: 4,
         image: "./assets/project_image/shop_porul.png",
         projectName: "Shop Porul (E-commerce site)",
         projectAbout: "This project is a functional front-end e-commerce site built with pure HTML, CSS, and JavaScript. Through this project, I learned how to create dynamic web pages and manage state using JavaScript.",
@@ -48,7 +48,7 @@ const projects = [
         githubLink: "https://github.com/Nishanthnaa52/Shop_Porul"
     },
     {
-        id: 4,
+        id: 5,
         image: "./assets/project_image/rock_paper_scissors.png",
         projectName: "Rock Paper Scissors Game",
         projectAbout: "This project is a Rock Paper Scissors game built with HTML, CSS, and JavaScript. Through this project, I learned how to manipulate the DOM and manage user interactions using JavaScript.",
@@ -56,14 +56,14 @@ const projects = [
         githubLink: "https://github.com/Nishanthnaa52/rock_paper_scissors_project"
     },
     {
-        id: 5,
+        id: 6,
         image: "./assets/project_image/sdal.png",
         projectName: "SDAL (Basic Desktop assistant)",
         projectAbout: "This project is a basic desktop assistant built with Python.",
         githubLink: "https://github.com/Nishanthnaa52/SDAL"
     },
     {
-        id: 6,
+        id: 7,
         image: "./assets/project_image/tic_tac_toe.png",
         projectName: "Tic Tac Toe Game",
         projectAbout: "This project is a Tic Tac Toe game built with HTML, CSS, and JavaScript.",
@@ -71,7 +71,7 @@ const projects = [
         githubLink: "https://github.com/Nishanthnaa52/tic_tac_toe_js"
     },
     {
-        id: 7,
+        id: 8,
         image: "./assets/project_image/youtube_clone.png",
         projectName: "Youtube Clone",
         projectAbout: "This project is a Youtube Clone built with pure HTML and CSS.",
@@ -79,7 +79,7 @@ const projects = [
         githubLink: "https://github.com/Nishanthnaa52/_youtube_clone_"
     },
     {
-        id: 8,
+        id: 9,
         image: "./assets/project_image/Calculator.png",
         projectName: "Calculator",
         projectAbout: "This project is a Calculator built with HTML, CSS, and JavaScript.",
@@ -87,7 +87,7 @@ const projects = [
         githubLink: "https://github.com/Nishanthnaa52/Simple_Calculator"
     },
     {
-        id: 9,
+        id: 10,
         image: "./assets/project_image/portfolio.png",
         projectName: "Amazing Portfolio",
         projectAbout: "This project is my portfolio built with React and Tailwind CSS.",
